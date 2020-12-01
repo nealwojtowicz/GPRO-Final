43a46d2421fce7acedde5ccd39a55d20584af8f2
neal.wojtowicz <neal.wojtowicz@mymail.champlain.edu>
2020-12-01 14:03:54 -0500

    Starting orange cube code for final

--- Final/shaders/TestPS.glsl
+++ Final/shaders/TestPS.glsl
@@ -1,1 +1,22 @@
-// empty shader file
+#version 450
+
+#ifdef GL_ES
+precision highp float;
+#endif
+
+layout (location = 0) out vec4 rtFragColor;
+
+uniform sampler2D uTex;
+uniform vec2 uResolution;
+
+in vec2 vTexcoord;
+in vec4 vPos;
+in vec4 vColor;
+in vec4 vNormal;
+in vec4 vLight;
+in float vIntensity;
+
+void main()
+{
+	rtFragColor = vec4(1.0, 0.5, 0.0, 1.0);
+}
--- Final/shaders/TestVS.glsl
+++ Final/shaders/TestVS.glsl
@@ -1,1 +1,21 @@
-// empty shader file
+#version 450
+
+layout (location = 0) in vec4 aPos;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoord;
+
+uniform mat4 uModelMatrix, uViewMatrix, uProjMatrix;
+
+out vec2 vTexcoord;
+out vec4 vPos;
+out vec4 vColor;
+out vec4 vNormal;
+out vec4 vLight;
+out float vIntensity;
+
+void main()
+{
+	mat4 modelViewProjMatrix = uProjMatrix * uViewMatrix * uModelMatrix;
+	vPos = modelViewProjMatrix * aPos;
+	gl_Position = vPos;
+}
