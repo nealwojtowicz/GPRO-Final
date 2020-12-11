81a3067c2e13a44bee01b89869557e59f914f425
neal.wojtowicz <neal.wojtowicz@mymail.champlain.edu>
2020-12-11 09:09:11 -0500

    Commit

--- Final/shaders/Final_pScenePS.glsl
+++ Final/shaders/Final_pScenePS.glsl
@@ -13,19 +13,49 @@ uniform bool uIsWall;
 
 in vec2 vTexcoord;
 in vec4 vPos;
+in vec4 vColor;
+in vec4 vNormal;
+in vec4 vLight;
+in float vIntensity;
+in vec4 aPosition;
 
 void main()
 {
 	vec4 col;
+	vec4 phongModel;
 	
 	if(uIsWall)
 	{
 		col = texture(uTexWall, vTexcoord);
 	}
-	
 	else
 	{
 		col = texture(uTexFloor, vTexcoord);
 	}
-	rtFragColor = col;
+
+	vec4 N = normalize(vNormal);
+	vec4 L = normalize(vLight - vPos);
+	float diffCoe = max(0.0, dot(N, L));
+	float dist = dot(vLight, vPos);
+	float atten = 1.0 / (1.0 + dist/vIntensity + (dist * dist)/(vIntensity * vIntensity));
+	float diffInt = diffCoe * atten;
+	vec3 H1 = normalize(L.xyz * L.xyz);
+	float spec = max(0.0, dot(N.xyz, H1));
+	phongModel = vec4(0.0) + diffInt * N * vColor + pow(spec, 64.0) * vec4(1.0);
+	
+	/*
+	vec4 view = normalize(vPos - aPosition);
+	vec4 light = normalize(vLight - aPosition);
+	vec4 normal = normalize(vNormal);
+	vec4 reflection = reflect(-light, normal);
+	
+	float diffcoe = max(0.0, dot(light, normal));
+	float dist = distance(vLight, aPosition);
+	float atten = 1.0 / (1.0 + dist/vIntensity + (dist * dist)/(vIntensity * vIntensity));
+	
+	vec4 spec = pow(max(0.0, dot(view, reflection)), 64.0) * vec4(1.0);
+	
+	phongModel = (vec4(0.0) + ((diffcoe + spec) * atten)) * normal * vColor; */
+	
+	rtFragColor = phongModel * col;
 }
--- Final/shaders/Final_pSceneVS.glsl
+++ Final/shaders/Final_pSceneVS.glsl
@@ -1,6 +1,7 @@
 #version 450
 
 layout (location = 0) in vec4 aPos;
+layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aTexCoord;
 
 uniform mat4 uModelMatrix, uViewMatrix, uProjMatrix;
@@ -8,11 +9,27 @@ uniform vec2 uScale;
 
 out vec2 vTexcoord;
 out vec4 vPos;
+out vec4 vColor;
+out vec4 vNormal;
+out vec4 vLight;
+out float vIntensity;
+out vec4 aPosition;
 
 void main()
 {
 	mat4 modelViewProjMatrix = uProjMatrix * uViewMatrix * uModelMatrix;
+	mat4 modelViewMatrix = uViewMatrix * uModelMatrix;
 	vPos = modelViewProjMatrix * aPos;
 	gl_Position = vPos;
 	vTexcoord = aTexCoord * uScale;
+	
+	vec4 lightPos = vec4(0.0, 0.0, 2.0, 1.0);
+	mat3 normalMat = inverse(transpose(mat3(modelViewMatrix)));
+	vec3 nrm = normalMat * aNormal;
+	
+	vNormal = vec4(nrm, 0.0);
+	vIntensity = 1.0;
+	vLight = lightPos;
+	vColor = aPos;
+	aPosition = aPos;
 }
